800cf8b340a850e3e3a391312c9a3eba31251d1a
MaciasMucias <maciej2.karaskiewicz@gmail.com>
2020-08-17 15:43:33 +0200

    Added Build to repo

---
+++ build/life_detection/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "life_detection"
+PROJECT_SPACE_DIR = "/home/plugit/lidar_mapping/devel/.private/life_detection"
+PROJECT_VERSION = "0.0.0"
